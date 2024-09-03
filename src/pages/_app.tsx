a0bcb6fa11742de442ba846a6fd2ed30b27b9cff
eduardoMessKL <eduardomk408@gmail.com>
2024-09-03 13:35:16 -0300

    Adding font "Jua"

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Jua } from "@next/font/google";
+
+const jua = Jua({
+   weight: "400", 
+   subsets: ["latin"],
+  });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={jua.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
